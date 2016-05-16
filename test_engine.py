24ddf4fbdd9227f3a4257ac4cebbff5b1d4c9cb8
Kyle Moad <Kyle Moad>
2016-05-16 15:53:31 -0400

    changed key files to csv

--- test_engine.py
+++ test_engine.py
@@ -34,7 +34,7 @@ print 'Test Started'
 # Initialize with options
 main_dir = 'C:/Users/Kyle/cravat/testing/test_cases'
 url = 'http://192.168.99.100:8888/CRAVAT'
-test_cases = ['oncogenes'] # Input tests to run as list of strings, or use 'all' to run every test in directory
+test_cases = ['all'] # Input tests to run as list of strings, or use 'all' to run every test in directory
 
 # Generate list of tests to run, either from dir names in main dir, or user input
 if test_cases == ['all']:
