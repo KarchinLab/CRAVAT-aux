ae21215674c40d4caa5d31c39b816fdfa6a5b69a
kmoad <kmoad@insilico.us.com>
2017-06-20 17:39:51 -0400

    test case cleanup
    removed: old test backups, redundant vcf tests, webservice tests
    moved: new_test.ps1 to maintenance

--- maintenance/cravat_to_vcf.py
+++ maintenance/cravat_to_vcf.py
@@ -3,6 +3,7 @@
 
 import os
 import shutil
+import sys
 
 def switch_strand(seq, strand):
     pairs = {'A':'T','T':'A','G':'C','C':'G'}
@@ -16,48 +17,49 @@ def switch_strand(seq, strand):
         out = seq
     return out
 
-c_test = 'seqont_c'
-top_dir = 'C:\\Users\\Kyle\\cravat\\CRAVAT-testing\\test_cases\\seqont'
-v_test = '_'.join(c_test.split('_')[:-1])+'_v'
-
-c_dir = os.path.join(top_dir, c_test)
-v_dir = os.path.join(top_dir, v_test)
-print 'src:', c_dir
-print 'dst:', v_dir
-
-print 'Making folder and files'
-if os.path.exists(v_dir):
-    cont = raw_input('VCF dir exists, continue? <y/n>: ')
-    if cont == 'y':
-        shutil.rmtree(v_dir)
+if __name__ == '__main__':
+    basedir = sys.argv[1]
+    cases = sys.argv[2].split(',')
+    for case in cases:
+        print 'Case:', case
+        c_test = case+'_c'
+        top_dir = os.path.join(basedir, case)
+        v_test = '_'.join(c_test.split('_')[:-1])+'_v'
+        
+        c_dir = os.path.join(top_dir, c_test)
+        v_dir = os.path.join(top_dir, v_test)
+        print 'src:', c_dir
+        print 'dst:', v_dir
+        
+        print '\tMaking folder and files'
+        if os.path.exists(v_dir):
+            shutil.rmtree(v_dir)
         os.makedirs(v_dir)
-    else:
-        exit()
-else:
-    os.makedirs(v_dir)
-
-shutil.copyfile(os.path.join(c_dir,'%s_desc.xml' %c_test), os.path.join(v_dir,'%s_desc.xml' %v_test))
-shutil.copyfile(os.path.join(c_dir,'%s_key.csv' %c_test), os.path.join(v_dir,'%s_key.csv' %v_test))
-shutil.copyfile(os.path.join(c_dir,'%s_input.txt' %c_test), os.path.join(v_dir,'%s_input.txt' %v_test))
-
-print '\tConverting input to VCF'
-input_path = os.path.join(v_dir,'%s_input.txt' %v_test)
-c_text = open(input_path).read()
-c_lines = c_text.split('\n')
-v_lines = []
-for c_line in c_lines:
-    c_vals = c_line.split('\t')
-    v_vals =['','','','','','25','PASS','BLANK']
-    v_vals[0] = c_vals[1]
-    v_vals[1] = c_vals[2]
-    v_vals[2] = c_vals[0]
-    v_vals[3] = switch_strand(c_vals[4],c_vals[3])
-    v_vals[4] = switch_strand(c_vals[5],c_vals[3])
-    v_lines.append('\t'.join(v_vals))
-v_text = '##fileformat=VCFv4.1\n#CHROM    POS    ID    REF    ALT    QUAL    FILTER    INFO\n'
-v_text += '\n'.join(v_lines)
-
-with open(os.path.join(v_dir,'%s_input.txt' %v_test),'w') as v_file:
-    v_file.write(v_text)
-print '\tCompleted'
-print 'All completed'
+        
+        shutil.copyfile(os.path.join(c_dir,'%s_desc.xml' %c_test), os.path.join(v_dir,'%s_desc.xml' %v_test))
+        shutil.copyfile(os.path.join(c_dir,'%s_key.csv' %c_test), os.path.join(v_dir,'%s_key.csv' %v_test))
+        shutil.copyfile(os.path.join(c_dir,'%s_input.txt' %c_test), os.path.join(v_dir,'%s_input.txt' %v_test))
+        
+        print '\tConverting input to VCF'
+        input_path = os.path.join(v_dir,'%s_input.txt' %v_test)
+        c_text = open(input_path).read()
+        c_lines = c_text.split('\n')
+        v_lines = []
+        for c_line in c_lines:
+            if not(c_line):
+                continue
+            c_vals = c_line.split('\t')
+            v_vals =['','','','','','25','PASS','BLANK']
+            v_vals[0] = c_vals[1]
+            v_vals[1] = c_vals[2]
+            v_vals[2] = c_vals[0]
+            v_vals[3] = switch_strand(c_vals[4],c_vals[3])
+            v_vals[4] = switch_strand(c_vals[5],c_vals[3])
+            v_lines.append('\t'.join(v_vals))
+        v_text = '##fileformat=VCFv4.1\n#CHROM    POS    ID    REF    ALT    QUAL    FILTER    INFO\n'
+        v_text += '\n'.join(v_lines)
+        
+        with open(os.path.join(v_dir,'%s_input.txt' %v_test),'w') as v_file:
+            v_file.write(v_text)
+        print '\tCompleted'
+    print 'All completed'
